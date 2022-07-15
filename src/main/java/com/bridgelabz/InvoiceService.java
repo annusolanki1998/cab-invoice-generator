f320a1af8485e0a2765a7191065bb56f05d74bba
Annu <annusinghraj1998@gmail.com>
2022-07-15 23:34:15 +0530

    ADDED: Created invoice generator which returned the total fare for the journey

--- src/main/java/com/bridgelabz/InvoiceService.java
+++ src/main/java/com/bridgelabz/InvoiceService.java
@@ -1,4 +1,12 @@
 package com.bridgelabz;
 
 public class InvoiceService {
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
 }
